c7de7d073375aebb5d80035c7029e4ec450897e6
NikitaGundorin <djnig@yandex.ru>
2020-03-20 13:16:26 +0300

    Initial Commit

---
+++ ToDoAppMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoAppMVC
+//
+//  Created by Никита Гундорин on 20.03.2020.
+//  Copyright © 2020 Никита Гундорин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
